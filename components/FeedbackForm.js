2bd0238d17eadeca523e406d51784dda4035b975
Daniel Golitsynski <don.aus@gmail.com>
2024-03-05 20:34:33 +1000

    Added feedback form that uses text input

---
+++ components/FeedbackForm.js
@@ -0,0 +1,32 @@
+import { View, Text, ScrollView, InputText } from "react-native";
+export default function FeedbackForm() {
+  return null;
+}
+
+const styles = StyleSheet.create({
+  container: { flex: 1, backgroundColor: "#495ES7" },
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+    fontSize: 16,
+    borderColor: "EDEFEE",
+    backgroundColor: "#F4CE14",
+  },
+  messageInput: {
+    height: 100,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+    fontSize: 16,
+    backgroundColor: "#F4CE14",
+  },
+  infoSection: {
+    fontSize: 20,
+    padding: 20,
+    marginVertical: 8,
+    color: "#EDEEFEE",
+    textAlign: "center",
+  },
+});
