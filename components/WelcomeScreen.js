0d5508b2faca0972efd1f59a2a8ac39608c460bc
Daniel Golitsynski <don.aus@gmail.com>
2024-03-06 19:08:03 +1000

    updated welcome screen to have text input for user name

--- components/WelcomeScreen.js
+++ components/WelcomeScreen.js
@@ -1,15 +1,38 @@
-import { Text, ScrollView, StyleSheet } from "react-native";
+import React, { useState } from "react";
+import {
+  Text,
+  ScrollView,
+  StyleSheet,
+  TextInput,
+  KeyboardAvoidingView,
+  Platform,
+} from "react-native";
 
 export default function WelcomeScreen() {
+  const [name, onChangeTextName] = useState("");
   return (
-    <ScrollView style={styles.scrollViewContainer} indicatorStyle="white">
-      <Text style={styles.headerText}>Welcome to Little Lemon</Text>
-      <Text style={styles.bodyText}>
-        Little Lemon is a charming neighborhood bistro that serves simple food
-        and classic cocktails in a lively but casual environment. We would love
-        to hear more about your experience with us!
-      </Text>
-    </ScrollView>
+    <KeyboardAvoidingView
+      style={styles.scrollViewContainer}
+      behavior={Platform.OS === "ios" ? "padding" : "height"}
+    >
+      <ScrollView
+        style={styles.scrollViewContainer}
+        indicatorStyle="white"
+        keyboardDismissMode="on-drag"
+      >
+        <Text style={styles.headerText}>Welcome to Little Lemon</Text>
+        <Text style={styles.bodyText}>
+          Little Lemon is a charming neighborhood bistro that serves simple food
+          and classic cocktails in a lively but casual environment. We would
+          love to hear more about your experience with us!
+        </Text>
+        <TextInput
+          style={styles.input}
+          input={name}
+          onChangeText={onChangeTextName}
+        ></TextInput>
+      </ScrollView>
+    </KeyboardAvoidingView>
   );
 }
 
@@ -27,5 +50,14 @@ const styles = StyleSheet.create({
     color: "white",
     marginVertical: 8,
     textAlign: "center",
+  },
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+    fontSize: 16,
+    borderColor: "#EDEFEE",
+    backgroundColor: "#F4CE14",
   },
 });
